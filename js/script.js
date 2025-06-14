dd894ccb3eaafa4517abfc55ab5d890dec6080cf
Ihor Moskva <imoskva2008@gmail.com>
2025-06-15 02:31:02 +0300

    created a contact section page

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* ==================================== typing animatoin ================================ */
+var typed = new Typed(".typing",{
+    strings:["Web Desainer", "Web oioiho", "supermen"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+
